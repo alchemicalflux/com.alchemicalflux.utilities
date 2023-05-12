cc883cd8931330a24c041a4a203d76522d44450e
alchemicalflux <126114445+alchemicalflux@users.noreply.github.com>
2023-05-12 00:22:05 -0700

    fix: Add unit tests and update StringManipulation
    Created unit tests for StringManipulation and used them to correct issues in the original functionality.

---
+++ Tests/Editor/Helpers/StringManipulationTests.cs
@@ -0,0 +1,118 @@
+using AlchemicalFlux.Utilities.Helpers;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace AlchemicalFlux.Tests
+{
+    public class StringManipulationTests
+    {
+        [Test]
+        public void MultipleReplace_ReplacesMultipleKeysWithValues_ReturnsReplacedString()
+        {
+            // Arrange
+            string input = "Hello [FirstName], you are from [Country].";
+            var replacements = new Dictionary<string, string>
+            {
+                { "[FirstName]", "John" },
+                { "[Country]", "USA" }
+            };
+
+            // Act
+            string result = StringManipulation.MultipleReplace(input, replacements);
+
+            // Assert
+            Assert.AreEqual("Hello John, you are from USA.", result);
+        }
+
+        [Test]
+        public void MultipleReplace_NoMatchingKeys_ReturnsUnchangedString()
+        {
+            // Arrange
+            string input = "Hello [FirstName], you are from [Country].";
+            var replacements = new Dictionary<string, string>
+            {
+                { "[Name]", "John" },
+                { "[Nation]", "USA" }
+            };
+
+            // Act
+            string result = StringManipulation.MultipleReplace(input, replacements);
+
+            // Assert
+            Assert.AreEqual(input, result);
+        }
+
+        [Test]
+        public void RegexMultipleReplace_ReplacesMultipleKeysWithValues_ReturnsReplacedString()
+        {
+            // Arrange
+            string input = "Hello {FirstName}, you are from {Country}.";
+            var replacements = new Dictionary<string, string>
+            {
+                { "{FirstName}", "John" },
+                { "{Country}", "USA" }
+            };
+
+            // Act
+            string result = StringManipulation.RegexMultipleReplace(input, replacements);
+
+            // Assert
+            Assert.AreEqual("Hello John, you are from USA.", result);
+        }
+
+        [Test]
+        public void RegexMultipleReplace_UsingRegexPattern_ReturnsReplacedString()
+        {
+            // Arrange
+            string input = "Hello {FirstName}, you are from {Country}.";
+            var replacements = new Dictionary<string, string>
+            {
+                { @"\{FirstName\}", "John" },
+                { @"\{Country\}", "USA" }
+            };
+
+            // Act
+            string result = StringManipulation.RegexMultipleReplace(input, replacements);
+
+            // Assert
+            Assert.AreEqual("Hello John, you are from USA.", result);
+        }
+
+        [Test]
+        public void RegexMultipleReplace_NoMatchingKeys_ReturnsUnchangedString()
+        {
+            // Arrange
+            string input = "Hello {FirstName}, you are from {Country}.";
+            var replacements = new Dictionary<string, string>
+            {
+                { "{Name}", "John" },
+                { "{Nation}", "USA" }
+            };
+
+            // Act
+            string result = StringManipulation.RegexMultipleReplace(input, replacements);
+
+            // Assert
+            Assert.AreEqual(input, result);
+        }
+
+        [Test]
+        public void RegexMultipleReplace_OverlappingKeys_ReturnLongestReplacementString()
+        {
+            // Arrange
+            string input = "The quick brown fox jumps over the lazy dog.";
+            var replacements = new Dictionary<string, string>
+            {
+                { "fox jumps", "cat leaps"},
+                { "jumps over", "hops across"},
+                { "fox jumps over", "squirrel scampers above"}
+            };
+
+            // Act
+            string result = StringManipulation.RegexMultipleReplace(input, replacements);
+
+            // Assert
+            Assert.AreEqual("The quick brown squirrel scampers above the lazy dog.", result);
+        }
+    }
+}
