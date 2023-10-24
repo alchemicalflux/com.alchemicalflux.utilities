e56c533b281a8fedaf1e686bbfc6f606003e6c93
alchemicalflux <126114445+alchemicalflux@users.noreply.github.com>
2023-10-24 12:39:08 -0700

    test: Update StringManipulator test suite to be more flexible
    Remove old test suite.
    Add generic suite for shared tests and functionality.
    Implement test suites and special cases for implementors of IStringManipulator.

---
+++ Tests/Editor/Scripts/Helpers/StringManipulator/BasicStringManipulatorTests.cs
@@ -0,0 +1,20 @@
+/*------------------------------------------------------------------------------
+  File:           BasicStringManipulatorTests.cs 
+  Project:        AlchemicalFlux Utilities
+  Description:    Unit tests for BasicStringManipulator and its unique cases.
+  Copyright:      ©2023 AlchemicalFlux. All rights reserved.
+
+  Last commit by: alchemicalflux 
+  Last commit at: 2023-10-24 12:39:09 
+------------------------------------------------------------------------------*/
+namespace AlchemicalFlux.Utilities.Helpers.Tests
+{
+    /// <summary>
+    /// Implements or overrides any unique test case scenerios for the 
+    /// <see cref="BasicStringManipulator"/>. class that are not covered by the 
+    /// <see cref="IStringManipulatorTests{}"/>.
+    /// </summary>
+    public class BasicStringManipulatorTests : IStringManipulatorTests<BasicStringManipulator>
+    {
+    }
+}
---
+++ Tests/Editor/Scripts/Helpers/StringManipulator/RegexStringManipulatorTests.cs
@@ -0,0 +1,121 @@
+/*------------------------------------------------------------------------------
+  File:           RegexStringManipulatorTests.cs 
+  Project:        AlchemicalFlux Utilities
+  Description:    Unit tests for RegexStringManipulator and its unique cases.
+  Copyright:      ©2023 AlchemicalFlux. All rights reserved.
+
+  Last commit by: alchemicalflux 
+  Last commit at: 2023-10-24 12:39:09 
+------------------------------------------------------------------------------*/
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace AlchemicalFlux.Utilities.Helpers.Tests
+{
+    /// <summary>
+    /// Implements or overrides any unique test case scenarios for the 
+    /// <see cref="RegexStringManipulator"/>. class that are not covered by the 
+    /// <see cref="IStringManipulatorTests{}"/>.
+    /// </summary>
+    public class RegexStringManipulatorTests : IStringManipulatorTests<RegexStringManipulator>
+    {
+        #region Members
+
+        #region Test Values
+
+        // Escape Characters
+        protected static readonly Dictionary<string, string> RegexCharactersReplacements = 
+            new Dictionary<string, string>()
+            {
+                { Regex.Escape("\\n"), "\n" },
+                { Regex.Escape("\\t"), "\t" },
+                { Regex.Escape("\\\\"), "\\" },
+            };
+
+        // Special Characters
+        protected static readonly Dictionary<string, string> RegexSpecialCharactersReplacements = 
+            new Dictionary<string, string>
+            {
+                { Regex.Escape("("), "[1]" },
+                { Regex.Escape(")"), "[2]" },
+                { Regex.Escape("{"), "[3]" },
+                { Regex.Escape("}"), "[4]" },
+            };
+
+        // Regex Replacement values
+        protected const string RegexText = "Valid \"version\": \"garbage\" Valid";
+        protected static readonly Dictionary<string, string> RegexReplacements = 
+            new Dictionary<string, string>()
+            {
+                { "\"version\": \".*\"", "\"version\": \"0.0.0-development\""},
+            };
+        protected const string RegexResults = "Valid \"version\": \"0.0.0-development\" Valid";
+
+        #endregion Test Values
+
+        #region Test Scenarios
+
+        // Test names
+        protected const string RegexReplacementsTestName = "RegexReplacements_ReturnModifiedString";
+
+        protected static readonly Dictionary<string, TestCaseData> RegexReplacementScenariosData =
+            new Dictionary<string, TestCaseData>()
+            {
+                {
+                    EscapeCharacterReplacementTestName,
+                    new TestCaseData(new RegexStringManipulator(),
+                        EscapeCharactersText, RegexCharactersReplacements, EscapeCharactersResult)
+                },
+                {
+                    SpecialCharacterReplacementsTestName,
+                    new TestCaseData(new RegexStringManipulator(),
+                        SpecialCharactersText, RegexSpecialCharactersReplacements, SpecialCharactersResult)
+                },
+                {
+                    RegexReplacementsTestName,
+                    new TestCaseData(new RegexStringManipulator(),
+                        RegexText, RegexReplacements, RegexResults)
+                },
+            };
+
+        #endregion Test Scenarios
+
+        #endregion Members
+
+        #region Methods
+
+        #region Test Case Sources
+
+        public static IEnumerable<TestCaseData> RegexReplacementScenarios()
+        {
+            var merged = RegexReplacementScenariosData.Merge(ReplacementScenariosData);
+            foreach (var scenario in merged)
+            {
+                yield return SetTestName(scenario.Value, scenario.Key);
+            }
+        }
+
+        #endregion Test Case Sources
+
+        #region Unit Tests
+
+        [Test]
+        [TestCaseSource(nameof(RegexReplacementScenarios))]
+        public override void MultipleReplace_StringReplacementTests(IStringManipulator stringManipulator,
+            string input,
+            Dictionary<string, string> replacements,
+            string expectedResult)
+        {
+            // Act
+            string result = stringManipulator.MultipleReplace(input, replacements);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        #endregion Unit Tests
+
+        #endregion Methods
+    }
+}
