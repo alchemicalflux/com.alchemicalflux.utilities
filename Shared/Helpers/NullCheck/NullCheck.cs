67bc2be4421390ca015f83b0842135109e678298
alchemicalflux <126114445+alchemicalflux@users.noreply.github.com>
2024-02-07 06:40:07 -0800

    feat: Add NullCheck attribute and its property drawer

---
+++ Shared/Helpers/NullCheck/NullCheck.cs
@@ -0,0 +1,26 @@
+/*------------------------------------------------------------------------------
+  File:           NullCheck.cs 
+  Project:        AlchemicalFlux Utilities
+  Description:    Property attribute alerting users when object field is null.
+  Copyright:      ©2024 AlchemicalFlux. All rights reserved.
+
+  Last commit by: alchemicalflux 
+  Last commit at: 2024-02-07 06:40:08 
+------------------------------------------------------------------------------*/
+using System;
+using UnityEngine;
+
+namespace AlchemicalFlux.Utilities.Helpers
+{
+    /// <summary>
+    /// Indicates that the associated field cannot be left as null within the Unity
+    ///   inspector.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
+    public class NullCheck : PropertyAttribute 
+    {
+
+        /// <summary>Indicates if the NullCheck should be ignored when assoiated with prefabs.</summary>
+        public bool IgnorePrefab { get; set; }
+    }
+}
