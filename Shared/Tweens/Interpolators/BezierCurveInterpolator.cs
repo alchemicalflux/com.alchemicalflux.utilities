f48884cedf30c29798af7a07bcfeef2a5e0d52fd
alchemicalflux <126114445+alchemicalflux@users.noreply.github.com>
2025-02-24 00:51:58 -0800

    feat: Implement Vector2 Bezier curve interpolation

--- Shared/Tweens/Interpolators/BezierCurveInterpolator.cs
+++ Shared/Tweens/Interpolators/BezierCurveInterpolator.cs
@@ -5,9 +5,10 @@ Overview:   Abstract base class for interpolations using a Bezier Curve.
 Copyright:  2025 AlchemicalFlux. All rights reserved.
 
 Last commit by: alchemicalflux 
-Last commit at: 2025-02-23 23:22:42 
+Last commit at: 2025-02-24 00:51:59 
 ------------------------------------------------------------------------------*/
 using AlchemicalFlux.Utilities.Math;
+using System;
 using System.Collections.Generic;
 using System.Numerics;
 
@@ -21,71 +22,122 @@ namespace AlchemicalFlux.Utilities.Tweens
     {
         #region Fields
 
-        private List<float> _tempMults = new();
-        private IReadOnlyList<BigInteger> _pascalTriangleRow;
-        private int _nodeCount = int.MinValue;
+        /// <summary>
+        /// Tracks the currently configured node count setting.
+        /// </summary>
+        protected int _nodeCount = int.MinValue;
 
         #endregion Fields
 
         #region Properties
 
+        /// <summary>
+        /// List of nodes to be used to generate the Bezier curve.
+        /// </summary>
         public IList<TType> Nodes { get; set; }
+
+        /// <summary>
+        /// List of Pascal's Triangle values for a given row of the triangle.
+        /// </summary>
+        protected IReadOnlyList<BigInteger> PascalTriangleRow { get; set; }
+
+        /// <summary>
+        /// A permanent list for generating multipliers for interpolation.
+        /// </summary>
+        protected List<float> TempMults { get; set; }
 
         #endregion Properties
 
         #region Methods
 
+        /// <summary>
+        /// Parameterless constructor for the BezierCurveInterpolator class.
+        /// </summary>
         public BezierCurveInterpolator()
         {
+            TempMults = new();
         }
 
+        /// <summary>
+        /// Parametered constructor for the BezierCurveInterpolator class.
+        /// </summary>
+        /// <param name="nodes">
+        /// Reference to the list of nodes for generating the Bezier curve.
+        /// </param>
         public BezierCurveInterpolator(IList<TType> nodes) : base()
         {
             Nodes = nodes;
         }
 
+        /// <inheritdoc />
         public virtual TType Interpolate(float progress)
         {
             if(Nodes.Count != _nodeCount) { RebuildNodes(); }
             if(_nodeCount == 0) { return default; }
 
-            var invProg = 1 - progress;
-            var mult = 1f;
-            for(var index = 0; index < _nodeCount; ++index, mult *= progress)
-            {
-                _tempMults[index] = mult;
-            }
-            mult = invProg;
-            for(var index = 2; index <= _nodeCount; ++index, mult *= invProg)
-            {
-                _tempMults[^index] *= mult;
-            }
+            GenerateInterpolationMultipliers(progress, 1 - progress);
 
-            TType result = default;
-            for(var index = 0; index < _nodeCount; ++index)
+            TType result = MultiplyBy(Nodes[0], TempMults[0]);
+            for(var index = 1; index < _nodeCount; ++index)
             {
-                _tempMults[index] *= (float)_pascalTriangleRow[index];
-                AddTo(ref result, MultiplyBy(Nodes[index], _tempMults[index]));
+                AddTo(ref result, MultiplyBy(Nodes[index], TempMults[index]));
             }
             return result;
         }
 
-        public abstract void AddTo(ref TType result, TType node);
-        public abstract TType MultiplyBy(TType node, float progress);
+        /// <summary>
+        /// Generates the multipliers for node interpolation.
+        /// </summary>
+        /// <param name="prog">Progress from 0.</param>
+        /// <param name="invProg">The opposite progression from 1.</param>
+        protected virtual void GenerateInterpolationMultipliers(float prog, 
+            float invProg)
+        {
+            TempMults[0] = (float)PascalTriangleRow[0];
+            var mult = prog;
+            for(var index = 1; index < _nodeCount; ++index, mult *= prog)
+            {
+                TempMults[index] = (float)PascalTriangleRow[index];
+                TempMults[index] *= mult;
+            }
+            mult = invProg;
+            for(var index = 2; index <= _nodeCount; ++index, mult *= invProg)
+            {
+                TempMults[^index] *= mult;
+            }
+        }
 
+        /// <summary>
+        /// Required function that adds the node's value to the result.
+        /// </summary>
+        /// <param name="result">TType value to be adjusted.</param>
+        /// <param name="node">TType value to be added.</param>
+        protected abstract void AddTo(ref TType result, TType node);
+
+        /// <summary>
+        /// Required function that multiplies the result by a fractional value.
+        /// </summary>
+        /// <param name="result">TType value to be multiplied.</param>
+        /// <param name="node">Value to be multiplied by.</param>
+        protected abstract TType MultiplyBy(TType node, float progress);
+
+        /// <summary>
+        /// Rebuilds TempMults and PascalTriangleRow to cover the current Nodes
+        /// size for safe interpolation calculations.
+        /// </summary>
         protected void RebuildNodes()
         {
             _nodeCount = Nodes.Count;
             if(_nodeCount == 0) { return; }
 
-            if(_tempMults.Count < _nodeCount)
+            if(TempMults.Count < _nodeCount)
             {
-                for(var iter = _tempMults.Count; iter < _nodeCount; ++iter)
+                for(var iter = TempMults.Count; iter < _nodeCount; ++iter)
                 {
-                    _tempMults.Add(0);
+                    TempMults.Add(0);
                 }
             }
-            _pascalTriangleRow = PascalsTriangle.GetRow(_nodeCount - 1);
+            PascalTriangleRow = PascalsTriangle.GetRow(_nodeCount - 1);
         }
 
         #endregion Methods
---
+++ Shared/Tweens/Vector/Vector2BezierCurveImpl.cs
@@ -0,0 +1,49 @@
+/*------------------------------------------------------------------------------
+File:       Vector2BezierCurveImpl.cs 
+Project:    AlchemicalFlux Utilities
+Overview:   Implements a Vector2 Bezier curve interpolation.
+Copyright:  2025 AlchemicalFlux. All rights reserved.
+
+Last commit by: alchemicalflux 
+Last commit at: 2025-02-24 00:51:59 
+------------------------------------------------------------------------------*/
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AlchemicalFlux.Utilities.Tweens
+{
+    /// <summary>
+    /// Class that implements a Vector2 Bezier curve interpolation.
+    /// </summary>
+    public sealed class Vector2BezierCurveImpl 
+        : BezierCurveInterpolator<Vector2>
+    {
+        #region Methods
+
+        /// <summary>
+        /// Constructor for the Vector2BezierCurveImpl class, which implements a
+        /// Vector2 Bezier curve interpolation.
+        /// </summary>
+        /// <param name="nodes">
+        /// Reference to the list of nodes for generating the Bezier curve.
+        /// </param>
+        public Vector2BezierCurveImpl(IList<Vector2> nodes)
+        {
+            Nodes = nodes;
+        }
+
+        /// <inheritdoc />
+        protected override void AddTo(ref Vector2 result, Vector2 node)
+        {
+            result += node;
+        }
+
+        /// <inheritdoc />
+        protected override Vector2 MultiplyBy(Vector2 node, float progress)
+        {
+            return node * progress;
+        }
+
+        #endregion Methods
+    }
+}
