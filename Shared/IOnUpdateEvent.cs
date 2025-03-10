78019eefb3582f769db0b65a68a8319e91cd987f
alchemicalflux <126114445+alchemicalflux@users.noreply.github.com>
2025-03-10 00:25:07 -0700

    feat: Add IOnUpdateEvent interface for handling update events
    - Introduced IOnUpdateEvent interface with methods for adding and removing update listeners.
    - Added XML documentation for the interface.

---
+++ Shared/IOnUpdateEvent.cs
@@ -0,0 +1,28 @@
+/*------------------------------------------------------------------------------
+File:       IOnUpdateEvent.cs 
+Project:    AlchemicalFlux Utilities
+Overview:   Interface for handling update events with a generic type.
+Copyright:  2025 AlchemicalFlux. All rights reserved.
+
+Last commit by: alchemicalflux 
+Last commit at: 2025-03-10 00:25:08 
+------------------------------------------------------------------------------*/
+using System;
+
+namespace AlchemicalFlux.Utilities.Events
+{
+    /// <summary>
+    /// Interface for handling update events with a generic type.
+    /// </summary>
+    /// <typeparam name="TType">The type of the value being updated.</typeparam>
+    public interface IOnUpdateEvent<TType>
+    {
+        /// <summary>Adds an update listener.</summary>
+        /// <param name="action">The action to be called on update.</param>
+        void AddOnUpdateListener(Action<TType> action);
+
+        /// <summary>Removes an update listener.</summary>
+        /// <param name="action">The action to be removed.</param>
+        void RemoveOnUpdateListener(Action<TType> action);
+    }
+}
