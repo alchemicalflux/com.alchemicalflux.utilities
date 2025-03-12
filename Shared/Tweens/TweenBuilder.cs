4693ca20efa2e410e0a11643f2fd33ee5aefa8c4
alchemicalflux <126114445+alchemicalflux@users.noreply.github.com>
2025-03-12 00:49:27 -0700

    fix; Add exception checks to TweenBuilder

--- Shared/Tweens/TweenBuilder.cs
+++ Shared/Tweens/TweenBuilder.cs
@@ -5,7 +5,7 @@ Overview:   Builder pattern wrapper for the BaseTween class.
 Copyright:  2025 AlchemicalFlux. All rights reserved.
 
 Last commit by: alchemicalflux 
-Last commit at: 2025-03-11 04:42:53 
+Last commit at: 2025-03-12 00:49:27 
 ------------------------------------------------------------------------------*/
 using System.Collections.Generic;
 using System;
@@ -18,9 +18,15 @@ namespace AlchemicalFlux.Utilities.Tweens
     /// <typeparam name="T">The type of the value being tweened.</typeparam>
     public class TweenBuilder<T> where T : IEquatable<T>
     {
+        #region Fields
+
         private readonly List<Action<T>> _updateActions = new();
         private IInterpolator<T> _interpolator;
         private Func<float, float> _easingFunction = Easings.Linear;
+
+        #endregion Fields
+
+        #region Methods
 
         /// <summary>
         /// Initializes a new instance of the <see cref="TweenBuilder{T}"/> 
@@ -33,8 +39,16 @@ namespace AlchemicalFlux.Utilities.Tweens
         /// class by copying another builder.
         /// </summary>
         /// <param name="builder">The builder to copy.</param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when the builder parameter is null.
+        /// </exception>
         public TweenBuilder(TweenBuilder<T> builder)
         {
+            if(builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
             _interpolator = builder._interpolator;
             _easingFunction = builder._easingFunction;
             _updateActions = new List<Action<T>>(builder._updateActions);
@@ -51,6 +65,9 @@ namespace AlchemicalFlux.Utilities.Tweens
         /// </summary>
         /// <param name="interpolator">The interpolator to use.</param>
         /// <returns>The current builder instance.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when the interpolator parameter is null.
+        /// </exception>
         public TweenBuilder<T> SetInterpolator(IInterpolator<T> interpolator)
         {
             _interpolator = interpolator ??
@@ -63,6 +80,9 @@ namespace AlchemicalFlux.Utilities.Tweens
         /// </summary>
         /// <param name="easingFunction">The easing function to use.</param>
         /// <returns>The current builder instance.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when the easingFunction parameter is null.
+        /// </exception>
         public TweenBuilder<T> SetEasing(Func<float, float> easingFunction)
         {
             _easingFunction = easingFunction ??
@@ -75,6 +95,9 @@ namespace AlchemicalFlux.Utilities.Tweens
         /// </summary>
         /// <param name="updateAction">The action to add.</param>
         /// <returns>The current builder instance.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when the updateAction parameter is null.
+        /// </exception>
         public TweenBuilder<T> AddUpdateAction(Action<T> updateAction)
         {
             if(updateAction == null)
@@ -89,13 +112,15 @@ namespace AlchemicalFlux.Utilities.Tweens
         /// Builds the tween with the specified parameters.
         /// </summary>
         /// <returns>A new <see cref="BaseTween{T}"/> instance.</returns>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown when the interpolator or easing function is not set.
+        /// </exception>
         public BaseTween<T> Build()
         {
             if(_interpolator == null)
             {
                 throw new InvalidOperationException(
                     "Interpolator must be set before building the tween.");
-
             }
             if(_easingFunction == null)
             {
@@ -110,5 +135,7 @@ namespace AlchemicalFlux.Utilities.Tweens
             }
             return tween;
         }
+
+        #endregion Methods
     }
 }
