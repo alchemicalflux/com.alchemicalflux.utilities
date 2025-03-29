0820e760879919ea7b44fd8720193eb79cacb72f
alchemicalflux <126114445+alchemicalflux@users.noreply.github.com>
2025-03-29 15:43:54 -0700

    refactor: Cleanup of WeightedIndexPool
    - Remove unneeded namespaces and properties
    - Add documentation to publicly accessible items.
    - Adjust to consistent format.

--- Shared/Helpers/WeightedIndexPool.cs
+++ Shared/Helpers/WeightedIndexPool.cs
@@ -7,11 +7,10 @@ Overview:   Provides a container for randomly selecting weighted indices while
 Copyright:  2025 AlchemicalFlux. All rights reserved.
 
 Last commit by: alchemicalflux 
-Last commit at: 2025-02-09 09:49:13 
+Last commit at: 2025-03-29 15:43:55 
 ------------------------------------------------------------------------------*/
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace AlchemicalFlux.Utilities.Helpers
 {
@@ -19,6 +18,9 @@ namespace AlchemicalFlux.Utilities.Helpers
     {
         #region Constants
 
+        /// <summary>
+        /// Reusable comparer for the binary search of any Data array.
+        /// </summary>
         private static readonly Comparer<Data> _comparer = Comparer<Data>
             .Create((x, y) => x.WeightRange.CompareTo(y.WeightRange));
 
@@ -26,10 +28,24 @@ namespace AlchemicalFlux.Utilities.Helpers
 
         #region Definitions
 
+        /// <summary>
+        /// Struct used to track pulled, discarded, and returned indices to the
+        /// pool. Struct used to ensure data is contiguous in memory.
+        /// </summary>
         private struct Data
         {
+            /// <summary>Value of the index for a given data entry.</summary>
             public int RefIndex;
+
+            /// <summary>
+            /// RefIndex's weight plus all accumulated weights from previous 
+            /// data entries when data entry is unlocked.
+            /// </summary>
             public double WeightRange;
+
+            /// <summary>
+            /// Locked indices are considered in use and cannot be shuffled.
+            /// </summary>
             public bool IsLocked;
         }
 
@@ -37,15 +53,13 @@ namespace AlchemicalFlux.Utilities.Helpers
 
         #region Fields
 
-        private Data[] _data;
-        private int[] _indexToData;
+        private readonly Data[] _data;
+        private readonly int[] _indexToData;
         private int _offset = 0;
 
         #endregion Fields
 
         #region Properties
-
-        public int Capacity { get; private set; }
 
         public int Count { get; private set; }
 
@@ -54,17 +68,25 @@ namespace AlchemicalFlux.Utilities.Helpers
         public Func<int, double> IndexToWeight { get; private set; }
         public Func<double> Randomizer { get; private set; }
 
+        private double PrevIndexWeight(int index) =>
+            (index == 0) ? 0 : _data[index - 1].WeightRange;
+
         #endregion Properties
 
         #region Methods
 
-        public WeightedIndexPool(int capacity, Func<int, double> indexToWeight,
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WeightedIndexPool"/> 
+        /// class.
+        /// </summary>
+        /// <param name="count">The number of indices in the pool.</param>
+        /// <param name="indexToWeight">
+        /// Function to determine the weight of an index.
+        /// </param>
+        /// <param name="randomizer">Function to generate random values.</param>
+        public WeightedIndexPool(int count, Func<int, double> indexToWeight,
             Func<double> randomizer)
         {
-            if(capacity <= 0)
-            {
-                throw new ArgumentOutOfRangeException(nameof(capacity));
-            }
             if(indexToWeight == null)
             {
                 throw new ArgumentNullException(nameof(indexToWeight));
@@ -74,23 +96,20 @@ namespace AlchemicalFlux.Utilities.Helpers
                 throw new ArgumentNullException(nameof(randomizer));
             }
 
-            Capacity = Count = capacity;
-            _data = new Data[Capacity];
-            _indexToData = new int[Capacity];
+            Count = count;
+            _data = new Data[Count];
+            _indexToData = new int[Count];
             IndexToWeight = indexToWeight;
             Randomizer = randomizer;
             Reset();
         }
 
-        public List<int> Add(int count)
-        {
-            return null;
-        }
+        #region Operations
 
-        //public void RemoveWeights(IEnumerable<int> indices)
-        //{
-        //}
-
+        /// <summary>
+        /// Pulls an index from the pool without replacement.
+        /// </summary>
+        /// <returns>The pulled index.</returns>
         public int PullIndex()
         {
             var dataIndex = PullIndexWithReplacement();
@@ -104,6 +123,10 @@ namespace AlchemicalFlux.Utilities.Helpers
             return _data[lastIndex].RefIndex;
         }
 
+        /// <summary>
+        /// Pulls an index from the pool with replacement.
+        /// </summary>
+        /// <returns>The pulled index.</returns>
         public int PullIndexWithReplacement()
         {
             if(_offset >= Count)
@@ -121,11 +144,19 @@ namespace AlchemicalFlux.Utilities.Helpers
             return dataIndex;
         }
 
+        /// <summary>
+        /// Discards an index from the pool.
+        /// </summary>
+        /// <param name="index">The index to discard.</param>
         public void DiscardIndex(int index)
         {
             MarkAsUnlocked(index);
         }
 
+        /// <summary>
+        /// Returns an index to the pool.
+        /// </summary>
+        /// <param name="index">The index to return.</param>
         public void ReturnIndex(int index)
         {
             var dataIndex = MarkAsUnlocked(index);
@@ -133,22 +164,25 @@ namespace AlchemicalFlux.Utilities.Helpers
             Swap(dataIndex, last);
             --_offset;
 
-            _data[last].WeightRange = IndexToWeight(_data[last].RefIndex) + 
+            _data[last].WeightRange = IndexToWeight(_data[last].RefIndex) +
                 PrevIndexWeight(last);
         }
 
+        /// <summary>
+        /// Shuffles the pool.
+        /// </summary>
         public void Shuffle()
         {
             // Find first unlocked index from end of pool.
             var right = Count - 1;
-            for(; right >= 0 && _data[right].IsLocked; --right) {}
+            for(; right >= 0 && _data[right].IsLocked; --right) { }
             if(right < 0) { return; } // No indices are unlocked for shuffle.
 
             // Group remaining locked indices to the end of the pool.
-            for(var left = 0; left < right; )
+            for(var left = 0; left < right;)
             {
-                for(; left < right && !_data[left].IsLocked; ++left) {}
-                for(; left < right && _data[right].IsLocked; --right) {}
+                for(; left < right && !_data[left].IsLocked; ++left) { }
+                for(; left < right && _data[right].IsLocked; --right) { }
                 if(left < right) { Swap(left, right); }
             }
             _offset = Count - ++right;
@@ -156,6 +190,14 @@ namespace AlchemicalFlux.Utilities.Helpers
             RebuildWeights(0, right);
         }
 
+        #endregion Operations
+
+        #region Accessing
+
+        /// <summary>
+        /// Gets the available indices in the pool.
+        /// </summary>
+        /// <returns>A list of available indices.</returns>
         public IList<int> AvailableIndices()
         {
             var available = Count - _offset;
@@ -167,19 +209,27 @@ namespace AlchemicalFlux.Utilities.Helpers
             return result;
         }
 
+        /// <summary>
+        /// Gets the pulled indices from the pool.
+        /// </summary>
+        /// <returns>A list of pulled indices.</returns>
         public IList<int> PulledIndices()
         {
             var result = new List<int>();
             for(var index = _offset; index > 0; --index)
             {
-                if(_data[^index].IsLocked) 
-                { 
-                    result.Add(_data[^index].RefIndex); 
+                if(_data[^index].IsLocked)
+                {
+                    result.Add(_data[^index].RefIndex);
                 }
             }
             return result;
         }
 
+        /// <summary>
+        /// Gets the discarded indices from the pool.
+        /// </summary>
+        /// <returns>A list of discarded indices.</returns>
         public IList<int> DiscardedIndices()
         {
             var result = new List<int>();
@@ -193,21 +243,9 @@ namespace AlchemicalFlux.Utilities.Helpers
             return result;
         }
 
-        public override string ToString()
-        {
-            var builder = new StringBuilder();
-            builder.AppendLine($"Capacity: {Capacity}, Count: {Count}, Offset: {_offset}");
-            for(var index = 0; index < Capacity; ++index)
-            {
-                var val = _data[index];
-                builder.AppendLine($"  " +
-                    $"Index:  {index}, " +
-                    $"RefIdx: {val.RefIndex}, " +
-                    $"WgtRng: {val.WeightRange}, " +
-                    $"IsRem:  {val.IsLocked}");
-            }
-            return builder.ToString();
-        }
+        #endregion Accessing
+
+        #region Helpers
 
         private void Reset()
         {
@@ -242,11 +280,6 @@ namespace AlchemicalFlux.Utilities.Helpers
             }
         }
 
-        private double PrevIndexWeight(int index)
-        {
-            return (index == 0) ? 0 : _data[index - 1].WeightRange;
-        }
-
         private int MarkAsUnlocked(int index)
         {
             var dataIndex = _indexToData[index];
@@ -258,6 +291,8 @@ namespace AlchemicalFlux.Utilities.Helpers
             _data[dataIndex].IsLocked = false;
             return dataIndex;
         }
+
+        #endregion Helpers
 
         #endregion Methods
     }
