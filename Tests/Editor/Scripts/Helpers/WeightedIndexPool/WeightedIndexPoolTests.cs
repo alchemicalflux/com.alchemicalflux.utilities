c0923620d84447ae95035a6c00441b1d05bea3f4
alchemicalflux <126114445+alchemicalflux@users.noreply.github.com>
2025-03-29 17:38:29 -0700

    test: Add test cases for WeightedIndexPool class
    - Added tests for DiscardIndex method to ensure an index can be discarded and is no longer considered locked.
    - Added tests for ReturnIndex method to ensure an index can be returned to the pool and is available for pulling again.
    - Added tests for Shuffle method to ensure the pool can be shuffled and indices are correctly grouped.

--- Tests/Editor/Scripts/Helpers/WeightedIndexPool/WeightedIndexPoolTests.cs
+++ Tests/Editor/Scripts/Helpers/WeightedIndexPool/WeightedIndexPoolTests.cs
@@ -5,7 +5,7 @@ Overview:   Test cases for the WeightedIndexPool class.
 Copyright:  2025 AlchemicalFlux. All rights reserved.
 
 Last commit by: alchemicalflux 
-Last commit at: 2025-03-29 05:13:51 
+Last commit at: 2025-03-29 17:38:29 
 ------------------------------------------------------------------------------*/
 
 using NUnit.Framework;
@@ -20,7 +20,7 @@ namespace AlchemicalFlux.Utilities.Helpers.Tests
     {
         #region Constants
 
-        private const int _maxCapacity = 10;
+        private const int _maxCapacity = 9;
 
         private double SingleWeight(int index) { return index; }
 
@@ -35,7 +35,7 @@ namespace AlchemicalFlux.Utilities.Helpers.Tests
         #region Constructors Tests
 
         [Test]
-        public void Constructor_CapacityZero_ThrowsArguementOutOfRangeException()
+        public void Constructor_CapacityZero_ThrowsArgumentOutOfRangeException()
         {
             // Assert
             Assert.Throws<ArgumentOutOfRangeException>(() =>
@@ -45,7 +45,7 @@ namespace AlchemicalFlux.Utilities.Helpers.Tests
         }
 
         [Test]
-        public void Constructor_CapacityNegative_ThrowsArguementOutOfRangeException()
+        public void Constructor_CapacityNegative_ThrowsArgumentOutOfRangeException()
         {
             // Assert
             Assert.Throws<ArgumentOutOfRangeException>(() =>
@@ -55,7 +55,7 @@ namespace AlchemicalFlux.Utilities.Helpers.Tests
         }
 
         [Test]
-        public void Constructor_NullIndexWeightFunctor_ThrowsArguementNullException()
+        public void Constructor_NullIndexWeightFunctor_ThrowsArgumentNullException()
         {
             // Assert
             Assert.Throws<ArgumentNullException>(() =>
@@ -65,7 +65,7 @@ namespace AlchemicalFlux.Utilities.Helpers.Tests
         }
 
         [Test]
-        public void Constructor_NullRandomizerFunctor_ThrowsArguementNullException()
+        public void Constructor_NullRandomizerFunctor_ThrowsArgumentNullException()
         {
             // Assert
             Assert.Throws<ArgumentNullException>(() =>
@@ -78,7 +78,7 @@ namespace AlchemicalFlux.Utilities.Helpers.Tests
         public void Count_Accessing_ReturnsInitialSize()
         {
             // Arrange
-            var pool = 
+            var pool =
                 new WeightedIndexPool(_maxCapacity, SingleWeight, DrawRandom);
 
             // Assert
@@ -109,7 +109,6 @@ namespace AlchemicalFlux.Utilities.Helpers.Tests
             // Assert
             Assert.IsFalse(pool.HasIndices);
         }
-
 
         #endregion Constructors Tests
 
@@ -245,6 +244,111 @@ namespace AlchemicalFlux.Utilities.Helpers.Tests
         }
 
         #endregion PullIndexWithReplacement Tests
+
+        #region DiscardIndex Tests
+
+        [Test]
+        public void DiscardIndex_DiscardPulledIndex_IndexIsUnlocked()
+        {
+            // Arrange
+            var pool =
+                new WeightedIndexPool(_maxCapacity, SingleWeight, DrawRandom);
+            var index = pool.PullIndex();
+
+            // Act
+            pool.DiscardIndex(index);
+
+            // Assert
+            var discardedIndices = pool.DiscardedIndices().ToList();
+            Assert.Contains(index, discardedIndices);
+        }
+
+        #endregion DiscardIndex Tests
+
+        #region ReturnIndex Tests
+
+        [Test]
+        public void ReturnIndex_ReturnPulledIndex_IndexIsAvailable()
+        {
+            // Arrange
+            var pool =
+                new WeightedIndexPool(_maxCapacity, SingleWeight, DrawRandom);
+            var index = pool.PullIndex();
+
+            // Act
+            pool.ReturnIndex(index);
+
+            // Assert
+            var availableIndices = pool.AvailableIndices().ToList();
+            Assert.Contains(index, availableIndices);
+        }
+
+        #endregion ReturnIndex Tests
+
+        #region Shuffle Tests
+
+        [Test]
+        public void Shuffle_ShufflePool_AvailableIndicesCountIsCorrect()
+        {
+            // Arrange
+            var pool =
+                new WeightedIndexPool(_maxCapacity, SingleWeight, DrawRandom);
+            for(var iter = 0; iter < _maxCapacity / 2; ++iter)
+            {
+                pool.PullIndex();
+            }
+
+            // Act
+            pool.Shuffle();
+
+            // Assert
+            var availableIndices = pool.AvailableIndices();
+            var availableCount = pool.Count - pool.PulledIndices().Count;
+            Assert.AreEqual(availableIndices.Count, availableCount);
+        }
+
+        [Test]
+        public void Shuffle_ShufflePool_PulledIndicesCountIsCorrect()
+        {
+            // Arrange
+            var pool =
+                new WeightedIndexPool(_maxCapacity, SingleWeight, DrawRandom);
+            var pulledIndices = new List<int>();
+            for(var iter = 0; iter < _maxCapacity / 2; ++iter)
+            {
+                pulledIndices.Add(pool.PullIndex());
+            }
+
+            // Act
+            pool.Shuffle();
+
+            // Assert
+            var shuffledPulledIndices = pool.PulledIndices();
+            Assert.AreEqual(shuffledPulledIndices.Count, _maxCapacity / 2);
+        }
+
+        [Test]
+        public void Shuffle_ShufflePool_PulledIndicesAreCorrect()
+        {
+            // Arrange
+            var pool =
+                new WeightedIndexPool(_maxCapacity, SingleWeight, DrawRandom);
+            var pulledIndices = new List<int>();
+            for(var iter = 0; iter < _maxCapacity / 2; ++iter)
+            {
+                pulledIndices.Add(pool.PullIndex());
+            }
+
+            // Act
+            pool.Shuffle();
+
+            // Assert
+            var shuffledPulledIndices = pool.PulledIndices();
+            CollectionAssert.AreEquivalent(pulledIndices, 
+                shuffledPulledIndices);
+        }
+
+        #endregion Shuffle Tests
 
         #endregion Methods
     }
