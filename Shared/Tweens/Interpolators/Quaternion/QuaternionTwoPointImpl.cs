cd24ac7a52ac83a5c79462a147670b0cb3507ad9
alchemicalflux <126114445+alchemicalflux@users.noreply.github.com>
2025-05-20 18:35:41 -0700

    refactor: Add intermediary abstract classes per type
    Covers Vector2/3 and Quaternion. Color requires future submission due to planned function signature change.

---
+++ Shared/Tweens/Interpolators/Quaternion/QuaternionTwoPointImpl.cs
@@ -0,0 +1,39 @@
+/*------------------------------------------------------------------------------
+File:       QuaternionTwoPointImpl.cs 
+Project:    AlchemicalFlux Utilities
+Overview:   Abstract base class for two-point quaternion interpolators. Provides
+            a foundation for interpolating between two Quaternion values.
+Copyright:  2025 AlchemicalFlux. All rights reserved.
+
+Last commit by: alchemicalflux 
+Last commit at: 2025-05-20 18:35:42 
+------------------------------------------------------------------------------*/
+using UnityEngine;
+
+namespace AlchemicalFlux.Utilities.Tweens
+{
+    /// <summary>
+    /// Abstract base class for two-point quaternion interpolators.
+    /// Provides a foundation for interpolating between two
+    /// <see cref="Quaternion"/> values.
+    /// </summary>
+    public abstract class QuaternionTwoPointImpl
+        : TwoPointInterpolator<Quaternion>
+    {
+        /// <summary>
+        /// Initializes a new instance of the
+        /// <see cref="QuaternionTwoPointImpl"/> class with the specified start
+        /// and end quaternions.
+        /// </summary>
+        /// <param name="start">
+        /// The initial quaternion value for interpolation.
+        /// </param>
+        /// <param name="end">
+        /// The final quaternion value for interpolation.
+        /// </param>
+        public QuaternionTwoPointImpl(Quaternion start, Quaternion end)
+            : base(start, end)
+        {
+        }
+    }
+}
---
+++ Shared/Tweens/Interpolators/Vector/Vector2TwoPointImpl.cs
@@ -0,0 +1,36 @@
+/*------------------------------------------------------------------------------
+File:       Vector2TwoPointImpl.cs 
+Project:    AlchemicalFlux Utilities
+Overview:   Abstract base class for two-point vector2 interpolators. Provides a
+            foundation for interpolating between two Vector2 values.
+Copyright:  2025 AlchemicalFlux. All rights reserved.
+
+Last commit by: alchemicalflux 
+Last commit at: 2025-05-20 18:35:42 
+------------------------------------------------------------------------------*/
+using UnityEngine;
+
+namespace AlchemicalFlux.Utilities.Tweens
+{
+    /// <summary>
+    /// Abstract base class for two-point vector2 interpolators. Provides a
+    /// foundation for interpolating between two <see cref="Vector2"/> values.
+    /// </summary>
+    public abstract class Vector2TwoPointImpl : TwoPointInterpolator<Vector2>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Vector2TwoPointImpl"/>
+        /// class with the specified start and end <see cref="Vector2"/> values.
+        /// </summary>
+        /// <param name="start">
+        /// The initial <see cref="Vector2"/> value for interpolation.
+        /// </param>
+        /// <param name="end">
+        /// The final <see cref="Vector2"/> value for interpolation.
+        /// </param>
+        public Vector2TwoPointImpl(Vector2 start, Vector2 end)
+            : base(start, end)
+        {
+        }
+    }
+}
